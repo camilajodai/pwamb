b95b52fe3b976fb653afb51e89043fdef1947d9c
Camila Jodai <camijodai@gmail.com>
2024-06-06 14:19:41 -0300

    Atividade

---
+++ mb.js
@@ -0,0 +1,7 @@
+document.getElementById('converter').addEventListener('click',
+function() {
+    const mb = document.getElementById('mb').value;
+    const gb = mb / 1024;
+
+    document.getElementById('result').innerText = `Resultado: ${gb.toFixed(1)} GB`;
+});
